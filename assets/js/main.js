43bf6448cbe5f684dc2f1d0e9b0a2c70f1fa227c
Ale-1407 <alessiacarrera07@gmail.com>
2022-12-15 19:22:27 +0100

    recupero dati api

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,13 @@
 var app = new Vue({
     el: '#app',
     data: {
-
+        arrayDischi: []
     },
     mounted(){
-
+        axios.get('./api/index.php')
+        .then( (res) => {
+            this.arrayDischi = res.data
+        })
     },
     methods:{
         
